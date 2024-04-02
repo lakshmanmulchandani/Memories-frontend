7a9642141ddb2287312fd5fe9299a4a95ff2b1fd
lakshmanmulchandani <mulchandanilakshman@gmail.com>
2024-04-02 17:56:26 +0530

    kaam kr ja

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,50 @@
 module.exports = {
-  "eslintConfig": {
-    "extends": "react-app"
-  }  
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // Set all rules to 'off' to ignore warnings
+    'import/extensions': 'off',
+    'react/prop-types': 'off',
+    'linebreak-style': 'off',
+    'react/state-in-constructor': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': 'off',
+    'no-multiple-empty-lines': 'off',
+    'no-underscore-dangle': 'off',
+    'object-curly-newline': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/alt-text': 'off',
+    'jsx-a11y/no-autofocus': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react/no-array-index-key': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/jsx-indent': 'off', // Turn off the indentation rule here
+    'no-unused-vars': 'off', // Turn off the unused vars rule
+    'eqeqeq': 'off', // Turn off the eqeqeq rule
+
+    // Add more rules here if needed
+  },
 };
